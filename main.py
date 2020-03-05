8b0dd71a68d78739ab63ebba3452c84f91196025
ThunderingWest4 <ThunderingWest4@github.com>
2020-03-05 09:26:35 -0500

    fprop nearly done, need to fix bias and sigmoidsum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sklearn import datasets
 import network
+import random
 
 iris = datasets.load_iris()
 irisdat = iris.data
@@ -9,5 +10,30 @@ numTypes = 3
 #total of 150 different things in the iris dataset
 #4 attributes
 #first 50 are setosa, second 50 are versicolour, last 50 are virginica
-NN = network.NeuralNetwork(len(irisdat[0]), 2, numTypes)
+NN = network.NeuralNetwork()
+NN.NeuralNetwork(len(irisdat[0]), 2, numTypes)
+val = []
 
+for i in range(len(irisdat)):
+    x = irisdat[i]
+    print(x)
+    u = [1]
+    for el in x:
+        u.append(el)
+    if(i<=50): 
+        val.append([u, 1])
+    elif(50 < i and i <= 100):
+        val.append([u, 2])
+    elif(100 < i and i <= 150):
+        val.append([u, 3])
+
+random.shuffle(val)
+
+print(val)
+
+training = val[0:124]
+testing = val[125:]
+
+NN.train(training, 2000)
+
+NN.test(testing)
--- network.py
+++ network.py
@@ -1,4 +1,6 @@
 import math
+import random
+import numpy as np
 
 class NeuralNetwork():
 
@@ -7,9 +9,80 @@ class NeuralNetwork():
         self.input = inputN
         self.hidden = hiddenL
         self.output = outputN
+        self.totalL = self.input + self.hidden + self.output
 
     def fProp(self):
         pass
+    
+    def train(self, data, iters):
+        self.ILweights = [[random.randint(-1000, 1000) for k in range(5)] for q in range(4)]
+        #5 branches from each node
+        #4 input nodes
+        #5 nodes in the hidden layer
+        #3 nodes in output layer, 3 choices
+        self.HLweights = [[random.randint(-1000, 1000) for k in range(3)] for q in range(5)]
+        #range(3) because 3 branches off
+        #q in range(5) because 5 nodes
+        weightedVals1 = []
+        weightedVals2 = []
+        """for i in range(iters):
+            for a in data:
+                LInput = a[0]
+                y = a[1]
+                for f in range(len(LInput)):
+                    weightedVals.append([LInput[f]*self.ILweights[f][w] for w in range(len(self.ILweights))])
+        """
+        for a in data:
+                LInput = a[0]
+                #already has a bias
+                y = a[1]
+                #y is the expected output/index of solution. 
+                #the output of nn will be a len3 array and the inds are 0, 1, 2
+                #y is the expected index of solution
+                temp = []
+                for f in range(len(LInput)):
+                    tem = self.ILweights[f]
+                    temp.append(sigmoid(sum([tem[i]*LInput[i] for i in range(len(LInput))])))
+                    print(temp)
+                    
+                weightedVals1.append(temp)
 
-def activation(x):
+                for t in weightedVals1:
+                        node = bias(t)                        
+                        #add bias
+                        print(node)
+                        temp2 = []
+                        for f in range(len(node)):
+                            temp.append(sigmoid(sum(np.dot(LInput, self.HLweights))))
+                        weightedVals2.append(temp2)
+
+                print("---------------------------------")
+                print(weightedVals1)
+                print(weightedVals2)
+        
+        #we almost have the fprop part down, next we need to work on backprop
+        
+
+    def test(self, testdata):
+        pass
+
+    def bProp(self):
+        pass
+
+def sigmoid(x):
     return (1 / (1 + (math.e ** (x*-1))))
+
+def maxIndex(arr):
+    ind = 0
+    prevmax = 0
+    for i in range(len(arr)):
+        if(arr[i] > prevmax):
+            ind = i
+            prevmax = arr[i]
+    return ind
+
+def bias(arr):
+    temp = [1]
+    for el in arr:
+        temp.append(el)
+    return temp
